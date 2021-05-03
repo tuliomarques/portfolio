550be5490a97b3c3fe483cd18a85924e36abd55d
Tulio Marques <tuliofmarques@gmail.com>
2021-05-03 20:52:33 +0100

    Background and head animation

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".hambuguer");
+
+hamburguer.addEventListener("click", function(){
+        document.querySelector(".sidebar").classList.toggle("show-menu");
+});
